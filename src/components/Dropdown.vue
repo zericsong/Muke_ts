cd643894ae80120d4f79ef429a711b31c5603df2
imzeric <realman@ZericSongdeMacBook-Air.local>
2022-02-13 15:15:30 +0800

    4-11

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownRef">
     <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false" @click.prevent="toggleOpen">
       {{title}}
     </a>
@@ -12,7 +12,9 @@
 
 
 <script lang="ts">
-import { defineComponent,PropType,ref } from "vue"
+import { defineComponent,ref,watch } from "vue"
+import useClickOutsides from "@/hooks/useClickOutsides"
+
 export default defineComponent({
   name: 'Dropdown',
   props:{
@@ -23,12 +25,22 @@ export default defineComponent({
   },
   setup(){
     const isOpen = ref(false)
+    const dropdownRef = ref<null | HTMLElement>(null)
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    
+    const isClickOutsides = useClickOutsides(dropdownRef)
+    watch(isClickOutsides,()=>{
+      if(isOpen.value && isClickOutsides.value) {
+        isOpen.value = false
+      }
+    })
+
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownRef,
     }
   }
 })
