40efc18c29c131075f1c045c05457d1adeee062e
superzeric <zericsong@126.com>
2022-06-14 23:42:51 +0800

    pinia

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router/index'
+import { useStore } from '@/store'
 
 const app = createApp(App)
 
 app.use(createPinia()).use(router).mount('#app')
 
+export const store = useStore()
 
+console.log('state',store.count)
+console.log('getter',store.add)
+console.log('getter',store.add)
+console.log('getter2',store.doubleCount)
 
 /* ------------------------------定义data ------------------------------*/
 /* ------------------------------定义data ------------------------------*/
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,22 @@
 import { defineStore } from 'pinia'
 
 export const useStore = defineStore('main', {
-  // other options...
-  
+  // arrow function recommended for full type inference
+  state: () => ({   //()立即返回函数，省略了return{...}
+    // all these properties will have their type inferred automatically
+    count: 1,
+  }),
+  getters: {
+    // automatically infers the return type as a number
+    add: (state) => ++state.count ,
+    doubleCount: (state) => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+    randomizeCounter() {
+      this.count = Math.round(100 * Math.random())
+    },
+  },
 })
